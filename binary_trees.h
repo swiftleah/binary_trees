042eb389384416a083517cc374425e5fb7c404d7
swiftleah <leahrosehenry@gmail.com>
2023-10-30 08:57:08 -0700

    Added brief header file

---
+++ binary_trees.h
@@ -0,0 +1,22 @@
+#ifndef BINARY_H
+#define BINARY_H
+
+#include <stdio.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+}
+
+#endif
