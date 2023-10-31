d3050429cd59a15c1a0320e8784fa5554f2ee22c
swiftleah <leahrosehenry@gmail.com>
2023-10-31 04:17:01 -0700

    Task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,23 +1,34 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * binary_tree_insert_left - inserts node to left-child of another node
+ * @parent: pointer to parent node to insert left-child in
+ * @value: value to store in new node
+ * Return: pointer to new node or NULL upon failure
  */
-int main(void)
-{
-    binary_tree_t *root;
 
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    binary_tree_print(root);
-    printf("\n");
-    binary_tree_insert_left(root->right, 128);
-    binary_tree_insert_left(root, 54);
-    binary_tree_print(root);
-    return (0);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	newnode = binary_tree_node(parent, value);
+
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+
+	if (parent->left != NULL)
+	{
+		newnode->left = parent->left;
+		parent->left->parent = newnode;
+	}
+
+	parent->left = newnode;
+	return (newnode);
 }
